64ea448b532358dea6564bfb98692945f9baf606
troller69iii <51433748+troller69iii@users.noreply.github.com>
2020-05-21 21:03:57 +0700

    update

--- app/src/main/java/vn/touchspace/example/ui/login/LoginActivity.java
+++ app/src/main/java/vn/touchspace/example/ui/login/LoginActivity.java
@@ -2,40 +2,17 @@ package vn.touchspace.example.ui.login;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
 import com.touchspace.example.R;
 import vn.touchspace.example.ui.base.BaseActivity;
 import vn.touchspace.example.ui.main.MainActivity;
 
 import javax.inject.Inject;
 
-import butterknife.BindView;
-import butterknife.OnClick;
-import vn.touchspace.example.utils.GsonUtils;
-
-public class LoginActivity extends BaseActivity implements LoginMvpView, View.OnClickListener {
-
-    @BindView(R.id.edt_email)
-    EditText edt_email;
-
-    @BindView(R.id.edt_password)
-    EditText edt_password;
+public class LoginActivity extends BaseActivity implements LoginMvpView {
 
     @Inject
     LoginMvpPresenter<LoginMvpView> mLoginPresenter;
 
-    private GoogleSignInClient mGoogleSignInClient;
-    private final int RC_SIGN_IN = 1000;
     private static final String TAG = "LoginActivity";
 
     @Override
@@ -52,46 +29,13 @@ public class LoginActivity extends BaseActivity implements LoginMvpView, View.On
 
         mLoginPresenter.onAttach(LoginActivity.this);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.server_client_id))
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        SignInButton signInButton = findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
-
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
-
-
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
-            // ...
-        }
-    }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
 
     @Override
     public void openMainActivity() {
         startActivity(new Intent(this, MainActivity.class));
         finish();
-    }
-
-    @OnClick(R.id.btn_login)
-    public void login() {
-        mLoginPresenter.onServerLoginClick(edt_email.getText().toString(),
-                edt_password.getText().toString());
     }
 
 
@@ -101,31 +45,4 @@ public class LoginActivity extends BaseActivity implements LoginMvpView, View.On
         super.onDestroy();
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            // Signed in successfully, show authenticated UI.
-            Log.w(TAG, "signInResult: " + account.getIdToken());
-            Log.w(TAG, "signInResult: " + GsonUtils.getInstance().toJson(account));
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-//            updateUI(null);
-        }
-    }
 }
