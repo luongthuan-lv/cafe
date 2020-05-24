606b881635785ae7ac05d502a8e972eaade43d4a
troller69iii <51433748+troller69iii@users.noreply.github.com>
2020-05-24 22:08:45 +0700

    no comment

--- app/src/main/java/vn/touchspace/example/ui/login/LoginActivity.java
+++ app/src/main/java/vn/touchspace/example/ui/login/LoginActivity.java
@@ -34,7 +34,6 @@ public class LoginActivity extends BaseActivity implements LoginMvpView {
     private static final String TAG = "LoginActivity";
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
